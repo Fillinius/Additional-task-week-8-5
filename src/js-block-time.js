df4b11c38b259b20d6bdb598dc647aff4e27f72a
Vladimir Revyagin <skorabeyf@gmail.com>
2023-01-20 15:13:18 +0300

    version release 0.1

--- src/js-block-time.js
+++ src/js-block-time.js
@@ -1,6 +1,9 @@
+import moment from "moment/moment"
+
 export function getBlockTime(){
   const date = new Date()
-    return date
+  const dateFormat =moment(date).format('MMMM Do YYYY, h:mm:ss a')
+    return dateFormat
 }
 // требуется поменять еще формат времени, но это уже в др итерации
 
